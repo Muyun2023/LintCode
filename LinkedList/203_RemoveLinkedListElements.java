9fd2921ab306ce0ed3ed664db71fea9498146a71
Muyun2023 <ji.mu@northeastern.edu>
2024-12-11 10:58:32 -0800

    Update 203_RemoveLinkedListElements.java

--- LinkedList/203_RemoveLinkedListElements.java
+++ LinkedList/203_RemoveLinkedListElements.java
@@ -1,18 +1,18 @@
 package LinkedList;
 
-public class 203_RemoveLinkedListElements {
-    public LinkedList RemoveLinkedListElements(LinkedList head,int target){
-    LinkedList dummy=new LinkedList(Integer.MIN_VALUE);
+class solution {
+    ListNode RemoveLinkedListElements(ListNode head,int target){
+    ListNode dummy=new ListNode(-1);
     dummy.next=head;
-    LinkedList cur=dummy;
+    ListNode cur=dummy;
     while(cur.next!=null){
-        if(cur.next.val=target){
+        if(cur.next.val==target){
             cur.next=cur.next.next;
         }else{
             cur=cur.next;
         }
     }
     return dummy.next;
-}
+    }
 
 }
